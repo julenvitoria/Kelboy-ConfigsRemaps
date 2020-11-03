c6162af5a24ec7d1420e172a7286a512e35e5e7f
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2020-11-03 10:58:13 +0100

    upload

--- ConfigsRemaps.sh
+++ ConfigsRemaps.sh
@@ -11,12 +11,12 @@ cd tmp
 #Downloading and copying emulators configs
 echo "Download and copy emulators configs"
 sleep 2
-~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/emulators
+~/scripts/github-downloader.sh https://github.com/julenvitoria/Kelboy-ConfigsRemaps/tree/master/emulators
 sudo cp -R emulators /opt/retropie/
 #Downloading and copying systems-games configs and remaps
 echo "Download and copy systems-games configs and remaps"
 sleep 2
-~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/configs
+~/scripts/github-downloader.sh https://github.com/julenvitoria/Kelboy-ConfigsRemaps/tree/master/configs
 cp -R configs /opt/retropie/
 cd ..
 rm -r tmp
