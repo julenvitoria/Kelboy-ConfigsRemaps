4aa66892f867137e5bdb48fae225cad417d6be38
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2020-11-03 10:57:40 +0100

    upload

---
+++ ConfigsRemaps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /home/pi
+if [ -d /home/pi/tmp ]; then
+        sudo rm -R /home/pi/tmp
+        mkdir /home/pi/tmp
+else
+        mkdir /home/pi/tmp
+fi
+cd tmp
+#Downloading and copying emulators configs
+echo "Download and copy emulators configs"
+sleep 2
+~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/emulators
+sudo cp -R emulators /opt/retropie/
+#Downloading and copying systems-games configs and remaps
+echo "Download and copy systems-games configs and remaps"
+sleep 2
+~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/configs
+cp -R configs /opt/retropie/
+cd ..
+rm -r tmp
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Kelboy-ConfigsRemaps
-Configs and remaps emulators
+Configs and remaps for emulators
+
+Repo to store the different game configurations under retroarch, reicast or any other emulator. Apart from controller configurations there are configurations of the emulators themselves. For example mupen64plus by default does not output audio, with this patch it will. 
+
+NOTE: IT'S NECCESARY THAT YOUR KELBOY HAS INTERNET CONNECTION.
